2fda2e8c5dcd110d45cc4151598a47fa3bc082b3
Xenon1345 <64515978+Xenon1345@users.noreply.github.com>
2023-11-10 20:53:47 +0800

    please rollback if this doesn't work

--- app/src/main/java/com/nushhack/keko/ScanFragment.kt
+++ app/src/main/java/com/nushhack/keko/ScanFragment.kt
@@ -6,13 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nushhack.keko.databinding.FragmentScanBinding
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.common.util.concurrent.ListenableFuture
 
 class ScanFragment : Fragment() {
     private var _binding: FragmentScanBinding? = null
     private val binding get() = _binding!!
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+        fun bindPreview(cameraProvider : ProcessCameraProvider) {
+            var preview : Preview = Preview.Builder()
+                .build()
+
+            var cameraSelector : CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+            preview.setSurfaceProvider(previewView.getSurfaceProvider())
+
+            var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+        }
+    }
+
+
 
 }
